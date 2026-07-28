76004ca8d9cd6719ad4cf77f093e052ddeba2b8e
kaifmohammedkhan <kaifkhanmohammed4@gmail.com>
2026-07-28 11:05:36 +0530

    DevSecOps GithubActions CI

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,7 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import Results from './pages/Results';
-import JobDetail from './pages/JobDetail';
-import About from './pages/About';       // ✅ New
-import Connect from './pages/Connect';   // ✅ New
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/results" element={<Results />} />
-        <Route path="/job/:id" element={<JobDetail />} />
-        <Route path="/about" element={<About />} />       {/* ✅ New */}
-        <Route path="/connect" element={<Connect />} />   {/* ✅ New */}
-        <Route path="*" element={<Home />} />
-      </Routes>
-    </Router>
-  );
+  return <Home />;
 }
 
 export default App;
